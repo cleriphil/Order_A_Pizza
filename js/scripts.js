aae480a91a3d1f1ae5fecf7660046a2c7e1a892c
cleriphil <cleri.phil@gmail.com>
2015-06-05 09:18:35 -0700

    Add initial files

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var Pizza = function(size) {
+  this.size = size;
+  this.toppings = []; //when the user selects toppings, it gets added
+                      //to this array
+};
+
+var Topping = function(name, cost) {
+
+};
---
+++ spec/specs.js
@@ -0,0 +1,4 @@
+describe('Pizza', function() {
+
+
+};
